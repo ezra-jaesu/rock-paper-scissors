499b57d3a4abbdd2aa0a95718b7dd32585f62107
jaesu <259886796+ezra-jaesu@users.noreply.github.com>
2026-03-27 15:38:25 +0800

    Implement computer choice using conditionals
    I successfully implemented getComputerChoice using conditionals.
    Initially, I was going to use an array since that is how it was
    usually implemented in tutorials but I've realized that we were
    not permitted to do and challenged myselfto solve it using
    conditionals.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!");
+// I was gonna use arrays but I've challenged myself that I will not use them.
+// The array method is more maintanable but for challenge's sake, let's go with conditionals.
+// const itemChoices = ["rock", "paper", "scissors"];
+
+let getComputerChoice = () => {
+  const randomDecimal = Math.random();
+  // I think this is technically bad practice? Clever code but unmaintainable.
+  // if value is 1.00 - 0.67, return rock
+  // elif value is 0.66 - 0.34, return paper
+  // else return scissors
+
+  return randomDecimal > 0.66
+    ? "rock"
+    : randomDecimal > 0.33
+      ? "paper"
+      : "scissors";
+};
