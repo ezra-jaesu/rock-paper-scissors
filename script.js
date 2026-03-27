b99bebec11f2785d7fe1e593d0819becc170d191
jaesu <259886796+ezra-jaesu@users.noreply.github.com>
2026-03-27 15:55:57 +0800

    Implement prompting the user for choice
    I have finished Step 3 and set a default value for the
    prompt using getComputerChoice. I have also discovered the beauty
    of arrow functions and refactored the code into a readable format.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ console.log("Hello, World!");
 // The array method is more maintanable but for challenge's sake, let's go with conditionals.
 // const itemChoices = ["rock", "paper", "scissors"];
 
+//  I just realized I can refactor both these functions to just return the value themselves.
+
+/*
 let getComputerChoice = () => {
   const randomDecimal = Math.random();
   // I think this is technically bad practice? Clever code but unmaintainable.
@@ -16,3 +19,19 @@ let getComputerChoice = () => {
       ? "paper"
       : "scissors";
 };
+
+let getHumanChoice = () => {
+  return prompt(
+    "Please pick one: rock, paper or scissors",
+    getComputerChoice(),
+  );
+};
+
+*/
+
+// This functoin is just so beatiful!!! SO SEXY!!!
+const getComputerChoice = (randomDecimal = Math.random()) =>
+  randomDecimal > 0.66 ? "rock" : randomDecimal > 0.33 ? "paper" : "scissors";
+
+const getHumanChoice = () =>
+  prompt("Please pick one: rock, paper or scissors", getComputerChoice());
